4eeb5d34dc2d4559b539c3c2da6a9b0ac1c39ef9
vendyv <33958819+vendyv@users.noreply.github.com>
2019-08-11 02:39:42 +0530

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -5,8 +5,10 @@ Created on Sun Aug 11 02:13:48 2019
 
 @author: vendy
 """
+#importing the module
 import pyqrcode
 
+#Function to generate the QR code
 def qrcode():
     q = pyqrcode.create('https://github.com/vendyv')
     q.png('gthb.png', scale=10)#, module_color=[0, 0, 0, 255], background=[0xff, 0xff, 0xcc])
